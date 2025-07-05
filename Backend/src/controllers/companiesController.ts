72fb02b27f1edceaef79c6a19bbbc5a96cbb7a64
Harry Larke <harrylarke1@hotmail.com>
2025-07-05 16:02:18 +0100

    companies controllers

--- Backend/src/controllers/companiesController.ts
+++ Backend/src/controllers/companiesController.ts
@@ -1,3 +1,4 @@
+import { compare } from "bcrypt";
 import { Request, Response } from "express";
 
 
@@ -5,8 +6,34 @@ import { Request, Response } from "express";
 export const getCompany = async (req: Request, res: Response) => {
     const { _id } = req.params
     if(!_id) {
-        res.status(500).json({messagee: 'Company ID is required.'})
+        res.status(400).json({messagee: 'Company ID is required.'})
         return
     }
-    res.json()
- }
+    const company = findOne(_id)
+    if(!company) {
+        res.status(204).json({message: `Cannot find company ${_id}`})
+    }
+    res.json(company)
+}
+
+export const addCompany = async (req: Request, res: Response) => {
+    const { name, employees, revenue, address, notes} = req.body
+    const newCompany = { name, employees, revenue, address, notes}
+    if(!name || employees?.employee) {
+        res.status(400).json({message: 'Each company requires a name and atleast one employee.'})
+        return
+    }
+    try{
+        await Company(newCompany)
+        res.sendStatus(201).json({message: `${name} added!`})
+    }catch(err: unknown | any ) {
+        res.sendStatus(500).json({message: `Unable to add company ${name}`})
+    } 
+}
+
+export const editCompany = async (req: Request, res: Response) => {
+    const { _id } = req.params 
+    //employees - wiill have to be an array? That should be fine?
+    const { name, employees, revenue, address, notes} = req.body
+
+}
